03b2f8823b27083235f7cf4e938807104a091683
Luca G. Soave <luca.soave@gmail.com>
2013-03-06 18:45:51 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soundcrowd::Application.config.secret_token = '1ba03e7f277d756d9700aa1dddf156ec9d2a8e9eb9bfc37295a319c234226e8f30016a8cc92af652cbd852734810b34bbf2cfebea1eedc8969bb6c8d2cf3eb66'
